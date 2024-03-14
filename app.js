1ec6f224f42104436a58a22674aae13828f06496
yancy <yancyshang@duck.com>
2024-03-14 12:20:08 +0800

    change port back to 8080

--- app.js
+++ app.js
@@ -453,7 +453,7 @@ process.on('uncaughtException', function (exception) {
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 443;
+var port = process.env.PORT || 8080;
 
 createServer({
     // 移除头部的Key
